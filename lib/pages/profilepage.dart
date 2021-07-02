9738a2a47cdeb4890410829f8aec7ffa35b45e64
Yadnesh <yadneshkhalane@gmail.com>
2021-07-02 15:49:56 +0530

    profilepage added

---
+++ lib/pages/profilepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(11.0),
+          child: Text('hello'),
+        ),
+      ),
+    );
+  }
+}
